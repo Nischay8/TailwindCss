d1093b5a15c75ffff2df9e83dce889b66fa8859a
Nischay Sinha <nischey.me008@gmail.com>
2024-11-03 01:28:34 +0530

    Added on 03 November 2024

--- ToDeskTop/script.js
+++ ToDeskTop/script.js
@@ -3,3 +3,11 @@ const navdialog=document.getElementById('nav-dialog');
 function handelMenu(){
     navdialog.classList.toggle('hidden');
 }
+
+
+function setUpInterSectionObserver(element,isLTR,speed){
+    const intersectionCallBack=(entries)=>{
+        
+    }
+    const intersectionobserver=new IntersectionObserver(intersectionCallBack);
+}
