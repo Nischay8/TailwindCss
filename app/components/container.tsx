4e6b195300c803412b1cabb2c23d7440b0f53c93
Core Techies <coretechiesofficial@gmail.com>
2026-01-05 18:55:28 +0530

    finta

---
+++ app/components/container.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Container = ({ children, className = "" }: ContainerProps) => {
+  return (
+    <div className={`max-w-6xl mx-auto px-4 md:py-6 ${className}`}>
+      {children}
+    </div>
+  );
+};
